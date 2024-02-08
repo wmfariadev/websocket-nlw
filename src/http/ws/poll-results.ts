68cdf38f05c53a197a57f7f917e692cd399852d6
William de Moraes Faria <william.faria@mult-e.com.br>
2024-02-08 12:20:49 -0300

    projeto websocket rocketseat nlw

---
+++ src/http/ws/poll-results.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from 'fastify'
+import z from 'zod'
+import { voting } from '../../utils/voting-pub-sub'
+
+export async function pollResults(app: FastifyInstance) {
+	app.get(
+		'/polls/:pollId/results',
+		{ websocket: true },
+		(connection, request) => {
+			const getPoolParams = z.object({
+				pollId: z.string().uuid(),
+			})
+
+			const { pollId } = getPoolParams.parse(request.params)
+
+			voting.subscribe(pollId, (message) => {
+				connection.socket.send(JSON.stringify(message))
+			})
+		},
+	)
+}
